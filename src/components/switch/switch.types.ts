4b15fda8e4c4320d9abd622e80ec69e59f1b127e
Makley <tibolamakley1@gmail.com>
2024-03-04 22:25:37 -0300

    fix: adjust import

--- src/components/switch/switch.types.ts
+++ src/components/switch/switch.types.ts
@@ -1,5 +1,6 @@
 import { ErrorMessages } from "~/lib/errorMessages";
 import { SetRequired } from "~/types/util.types";
+import { SwitchProps as NextUISwitchProps } from '@nextui-org/react'
 
 export type SwitchProps = SetRequired<NextUISwitchProps, 'name'> & {
   rules?: ErrorMessages;
